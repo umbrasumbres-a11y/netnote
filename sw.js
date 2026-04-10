d47f669705682e597a9adcf9d345386e2249e809
umbrasumbres-a11y <umbras.umbres@gmail.com>
2026-04-10 13:18:11 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'netnote-v3'; // Ubah versi cache agar browser memuat file baru
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest', // Tambahkan manifest
+  './icon-192.png',        // Tambahkan icon 192
+  './icon-512.png',        // Tambahkan icon 512
+  // CSS & Fonts
+  'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
+  // Gambar QRIS
+  'https://z-cdn-media.chatglm.cn/files/258f98b6-4146-4c1d-b6cc-7be09d423b3a.png?auth_key=1875795308-f74145f8ba7f486faac40f34872ba664-0-7865734052d32e2a21fa35050fd455c0'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Service Worker: Membuka Cache V3');
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then(fetchResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    }).catch(() => {
+      if (event.request.destination === 'document') {
+        return caches.match('./index.html');
+      }
+    })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
